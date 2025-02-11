ec241048df29732d85311ecc3d8b57c880af8f72
MohdSahbaz <cshabaz188@gmail.com>
2025-02-11 23:39:49 +0530

    add a loading spiner

--- clinet/src/components/common/loader/Loader.jsx
+++ clinet/src/components/common/loader/Loader.jsx
@@ -4,7 +4,9 @@ const Loader = () => {
   return (
     <div className="flex flex-col justify-center items-center h-64 space-y-4 animate-fade-in">
       <FaBookOpen className="text-emerald-600 animate-spin-slow text-5xl" />
-      <h1 className="text-xl animate-bounce">Loading Resources...</h1>
+      <div className="flex justify-center py-8">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-emerald-400"></div>
+      </div>
     </div>
   );
 };
