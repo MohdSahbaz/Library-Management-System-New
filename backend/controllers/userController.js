b353f4cacdd9eea20d12885e140551d628a7131d
MohdSahbaz <cshabaz188@gmail.com>
2025-02-24 12:55:28 +0530

    created profile controller etc

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,14 @@ const signIn = async (req, res) => {
 };
 
 const profile = async (req, res) => {
-  res.json({ message: "Protected Route", user: req.user });
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 module.exports = { signUp, signIn, profile };
