320a63e33ade55110a4bc9a86eb43bf242fce023
MohdSahbaz <cshabaz188@gmail.com>
2025-03-01 13:53:25 +0530

    add style

--- librarian/src/pages/dashboard/Card.jsx
+++ librarian/src/pages/dashboard/Card.jsx
@@ -1,13 +1,15 @@
 const Card = ({ icon, title, total }) => {
   return (
-    <article className="flex-1 basis-0 flex flex-col items-center p-6 px-4 bg-emerald-900/[0.5] shadow-md rounded-sm text-center border border-gray-200 transition-all duration-300">
+    <article className="group flex-1 basis-0 flex flex-col items-center p-6 px-4 bg-emerald-900/[0.5] hover:bg-emerald-950/[0.5] shadow-md rounded-sm text-center border border-gray-200 transition-all duration-300">
       <div className="flex items-center gap-2">
         <span className="text-4xl">{icon}</span>
-        <h2 className="text-xl font-semibold text-emerald-100 mt-2">
+        <h2 className="text-xl group-hover:text-emerald-50 font-semibold text-emerald-900 mt-2 transition-all duration-300">
           {title || "Unknown"}
         </h2>
       </div>
-      <p className="text-lg text-gray-600 mt-1">Total: {total || "0"}</p>
+      <p className="text-lg text-gray-600 group-hover:text-emerald-50/[0.5] mt-1 transition-all duration-300">
+        Total: {total || "0"}
+      </p>
     </article>
   );
 };
--- librarian/src/pages/dashboard/Dashboard.jsx
+++ librarian/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Header from "../../components/common/Header";
 import Card from "./Card";
-import { BookOpen } from "lucide-react";
+import { BookOpen, Users } from "lucide-react";
 
 const Dashboard = () => {
   useEffect(() => {
@@ -12,20 +12,16 @@ const Dashboard = () => {
     <>
       <Header pageName={"Dashboard"} />
       <div className="min-h-[calc(100vh-85px)] px-5">
-        <h1 className="mt-5">Books Detail</h1>
+        <h1 className="mt-5 text-xl">Books Detail</h1>
         <div className="flex gap-3 w-full flex-wrap text-white">
           <Card icon={<BookOpen size={20} />} title={"Total"} total={"1k"} />
           <Card icon={<BookOpen size={20} />} title={"Pending"} total={20} />
           <Card icon={<BookOpen size={20} />} title={"Borrowed"} total={500} />
           <Card icon={<BookOpen size={20} />} title={"Overdue"} total={10} />
         </div>
-        <h1 className="mt-5">Users Detail</h1>
+        <h1 className="mt-5 text-xl">Users Detail</h1>
         <div className="flex gap-3 w-full flex-wrap text-white">
-          <Card
-            icon={<BookOpen size={20} />}
-            title={"Total Users"}
-            total={"1k"}
-          />
+          <Card icon={<Users size={20} />} title={"Total"} total={"1k"} />
         </div>
       </div>
     </>
